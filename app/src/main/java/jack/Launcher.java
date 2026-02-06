c19068386962918e6eb0d9cbc7cbe4bebd8cadf9
SuperWhyNot <benjack538@gmail.com>
2026-02-07 01:41:48 +0800

    branch-A-Varargs

--- app/src/main/java/jack/Launcher.java
+++ app/src/main/java/jack/Launcher.java
@@ -7,6 +7,19 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Main.class, args);
+        boolean isGUI = true;
+        for (String arg : args) {
+            if (arg.equals("--command")) {
+                isGUI = false;
+                break;
+            } else if (arg.equals("-c")) {
+                isGUI = false;
+            }
+        }
+        if (isGUI) {
+            Application.launch(Main.class, args);
+        } else {
+            new Jack("./data/duke.txt").run();
+        }
     }
 }
